988f81e6b6d94bbca88c98a86705eaa1172f3dad
Nehemiah Kamolu <kamolunehemiah@gmail.com>
2022-04-27 11:24:35 +0000

    retreiving lists from redis

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -42,6 +42,28 @@ def call_history(method: Callable) -> Callable:
     return wrapper
 
 
+def replay(method: Callable) -> None:
+    """
+    display the history of calls of a particular function
+    """
+    key = method.__qualname__
+    key_inputs = key + ":inputs"
+    key_outputs = key + ":outputs"
+
+    redis = method.__self__._redis
+
+    count = redis.get(key).decode("utf-8")
+    print("{} was called {} times:".format(key, count))
+
+    inputList = redis.lrange(key_inputs, 0, -1)
+    outputList = redis.lrange(key_outputs, 0, -1)
+
+    all_data = list(zip(inputList, outputList))
+    for i, j in all_data:
+        attr, data = i.decode("utf-8"), j.decode("utf-8")
+        print("{}(*{}) -> {}".format(key, attr, data))
+
+
 class Cache:
     """
     A redis Cache class
